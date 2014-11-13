fd3970cc4afbc3a243683b4ad009709f152e0d99
cssmillie <cssmillie@gmail.com>
2014-11-13 14:51:34 -0500

    tseries

--- tseries.r
+++ tseries.r
@@ -15,7 +15,7 @@ read_tseries = function(fn, std=TRUE){
 fit_arima = function(x){
     require(forecast)
     # Fit ARIMA model to each column of x
-    y = as.data.frame(apply(x, 2, function(xi) fitted(auto.arima(xi
+    y = as.data.frame(apply(x, 2, function(xi) fitted(auto.arima(xi))))
     # Get residuals
     z = x - y
     return(list(fitted=y, residuals=z))
