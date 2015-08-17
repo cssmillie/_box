65063146acdc58c21f5f8e903e62b09a7d24529a
cssmillie <cssmillie@gmail.com>
2015-08-17 12:42:07 -0400

    small fixes

--- ssub.py
+++ ssub.py
@@ -86,7 +86,7 @@ def pbs_header(n_jobs, max_jobs=max_jobs, outfile='error', queue='short', memory
 def parse_args():
     
     # print usage statement
-    usage = "cat list_of_commands.txt | ssub -n 100 -q hour -G gscidfolk -m 8 --io 10\n"
+    usage = "\n\ncat list_of_commands.txt | ssub -n 100 -q hour -G gscidfolk -m 8 --io 10\n"
     usage +="ssub -n 100 -q long -m 8 'command 1; command 2; command 3;"
     
     # add command line arguments
